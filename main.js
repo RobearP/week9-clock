5b04db9cf850c5a171a3d7d587f4edfad8e35ba0
Siyu Duan <siyusiyuduan@hotmail.com>
2023-10-31 13:57:21 +0000

    clock working

--- main.js
+++ main.js
@@ -1,14 +1,39 @@
-const secondsHand = document.getElementsByClassName("hand");
-const minuteHand = document.getElementsByClassName("minute-hand");
-const hourHand = document.getElementsByClassName("hour-hand");
+const secondHand = document.querySelector(".hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 //PLAN
 //create a function loop (infinite) to move hand every 1 second
 function rotate(){
-    let seconds = 6;
-    secondsHand.style.rotate = `90deg`
+    const newDate = new Date();
+    const seconds = newDate.getSeconds();
+    const minutes = newDate.getMinutes();
+    const hours = newDate.getHours();
+    let secondsCurrent = 6*seconds;
+    let minutesCurrent = 6*minutes;
+    let hoursCurrent = 30*hours;
+    secondHand.style.rotate = `${secondsCurrent}deg`;
+    minuteHand.style.rotate = `${minutesCurrent}deg`;
+    hourHand.style.rotate = `${hoursCurrent}deg`;
+    // const secondTimer = setInterval(function() {
+    //     secondsCurrent += 6;
+    //     secondHand.style.rotate = `${secondsCurrent}deg`;
+    // }, 1000);
+    // const minuteTimer = setInterval(function () {
+    //     minutesCurrent += 6;
+    //     minuteHand.style.rotate = `${minutesCurrent}deg`;
+    // }, 60000);
+    // const hourTimer = setInterval(function () {
+    //     hoursCurrent += 30;
+    //     hourHand.style.rotate = `${hoursCurrent}deg`;
+    // }, 360000);
+    
 }
-setTimeout(rotate, 1000)
+
+setInterval(rotate, 1000);
+// document.addEventListener("load", function () {
+//     const clock = setInterval(rotate(), 1000);
+// })
 
 /*document.addEventListener("load", function(){
     console.log("hello world")
